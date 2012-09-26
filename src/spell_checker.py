f6d24511c868edb1c077656551cd6aec5a6aeea5
S Pradeep Kumar <gohanpra@gmail.com>
2012-09-26 12:56:25 +0530

    Added a dummy spell checker.

---
+++ src/spell_checker.py
@@ -0,0 +1,11 @@
+class SpellChecker(object):
+    """Suggest corrections for errors in queries.
+    """
+    
+    def __init__(self):
+        """Nothing much.
+        """
+        print 'Yo, boyz!'
+        
+if __name__ == '__main__':
+    spell_checker = SpellChecker ()
