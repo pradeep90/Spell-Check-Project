70bc90716e57ff23682352c12b81681d2f770c8b
S Pradeep Kumar <gohanpra@gmail.com>
2012-09-27 14:29:08 +0530

    Added code for calculating EP, ER, EF1.
    Also added code for run_spell_check and get_all_stats.

--- src/spell_checker.py
+++ src/spell_checker.py
@@ -1,19 +1,62 @@
+#!/usr/bin/python
+
+# Program to correct spelling errors.
+
+def get_EP(query_list, suggestion_dict, human_suggestion_dict):
+    """Return EP for the suggestions as judged by human_suggestion_dict.
+
+    EP: Expected Precision.
+    Average over queries in query_list of posteriors for those
+    suggestions that are in the human_suggestion_dict.
+    """
+    total = sum(sum(posterior
+                    for (suggestion, posterior) in suggestion_dict[query] 
+                    if suggestion in human_suggestion_dict[query])
+                for query in query_list)
+    return total / float(len(query_list))
+
+def get_ER(query_list, suggestion_dict, human_suggestion_dict):
+    """Return ER for the suggestions as judged by human_suggestion_dict.
+
+    ER: Expected Recall.
+    Average over queries in query_list of number of human suggestions
+    that have been suggested in suggestion_dict.
+    """
+    total = sum(sum(1
+                    for human_suggestion in human_suggestion_dict[query] 
+                    if human_suggestion in zip(*suggestion_dict[query])[0]
+                    ) / float(len(human_suggestion_dict[query]))
+                for query in query_list)
+    return total / float(len(query_list))
+            
+def get_HM(x, y):
+    """Return HM of x and y."""
+    return (2 * x * y) / (x + y)
+
+
 class SpellChecker(object):
     """Suggest corrections for errors in queries.
     """
-    
+
     def __init__(self):
-        """Nothing much.
+        """Initialize all the dicts for suggestions, etc.
         """
         print 'Yo, boyz!'
+        self.suggestion_dict = {}
 
-    def generate_suggestions(self, query):
-        """Return suggestions for query along with their posterior probabilities.
-
-        Return (Suggestions, Posteriors) where Suggestions is a list
-        of suggestions and Posteriors is a list of the corresponding
-        posterior probabilities.
+    def run_spell_check(self, query_list):
+        """Run spell check on queries in query_list and store the suggestions.
         
+        Arguments:
+        - `query_list`:
+        """
+        self.query_list = query_list
+        for query in self.query_list:
+            self.suggestion_dict[query] = self.generate_suggestions_and_posteriors(query)
+
+    def generate_suggestions_and_posteriors(self, query):
+        """Return (suggestion, posterior) pairs for query.
+
         Arguments:
         - `query`:
         """
@@ -22,12 +65,34 @@ class SpellChecker(object):
                        "liaison", "occasion", "possible", "throughout", 
                        "volley", "tattoos", "respect"]
 
-        posteriors = [0.69, 0.15, 0.93, 0.30, 0.30, 0.31, 0.15, 
-                      0.38, 0.48, 0.62, 0.11, 0.22, 0.44, 0.30, 0.82]
-        return (suggestions, posteriors)
+        posteriors = [0.11, 0.02, 0.15, 0.04, 0.04, 0.05, 0.02, 0.06, 0.07, 
+                      0.1, 0.01, 0.03, 0.07, 0.04, 0.19]
+        return zip(suggestions, posteriors)
 
-    # def evaluate_suggestions(query, suggestions, posteriors, human_suggestions)
+    def get_EF1_measure(self, human_suggestion_dict):
+        """Return EF1 value for the performance as judged by human_suggestion_dict.
+        
+        Arguments:
+        - `human_suggestion_dict`: dict of query -> list of
+          human-annotated suggestions
+        """
+        args = [self.query_list, self.suggestion_dict, human_suggestion_dict]
+        return get_HM (get_EP(*args),
+                       get_ER(*args))
+
+    def get_all_stats(self, human_suggestion_dict):
+        """Return [EP, ER, EF1] for performance as judged by human_suggestion_dict.
+        
+        Arguments:
+        - `human_suggestion_dict`:
+        """
+        args = [self.query_list, self.suggestion_dict, human_suggestion_dict]
+        return [get_EP(*args), get_ER(*args), 
+                self.get_EF1_measure(human_suggestion_dict)]
 
 if __name__ == '__main__':
     spell_checker = SpellChecker()
-    print spell_checker.generate_suggestions('yo')
+    spell_checker.run_spell_check(['yo', 'boyz'])
+    human_dict = { 'yo': ['bar', 'jack'], 'boyz': ['tattoos', 'respect', 'baz'] }
+    print spell_checker.get_EF1_measure(human_dict)
+    print spell_checker.get_all_stats(human_dict)
--- src/test_spell_checker.py
+++ src/test_spell_checker.py
@@ -1,17 +1,73 @@
+import spell_checker
 import unittest
 
 class SpellCheckerTest(unittest.TestCase):
     def setUp(self):
-        self.foo = 'Yo, Boyz'
+        self.spell_checker = spell_checker.SpellChecker()
 
     def tearDown(self):
         pass
 
-    def test_foo(self):
-        self.assertEqual(self.foo, 'Yo, Boyz')
+    def test_get_EP(self):
+        test_dict = { 1: [('foo', 0.3), ('bar', 0.7)], 
+                      2: [('baz', 0.8), ('bah', 0.2)]}
+        ans_dict = { 1: ['bar', 'jack'], 2: ['all', 'work', 'baz'] }
+        self.assertAlmostEqual(spell_checker.get_EP([1, 2], test_dict, ans_dict),
+                               0.75)
+        self.assertAlmostEqual(spell_checker.get_EP([1], test_dict, ans_dict),
+                               0.7)
 
-    def test_bar(self):
-        self.assertEqual(3 + 4, 7)
+    def test_get_ER(self):
+        test_dict = { 1: [('foo', 0.3), ('bar', 0.7)], 
+                      2: [('baz', 0.8), ('bah', 0.2)]}
+        ans_dict = { 1: ['bar', 'jack'], 2: ['all', 'work', 'baz'] }
+        self.assertAlmostEqual(spell_checker.get_ER([1, 2], test_dict, ans_dict),
+                               5.0/12)
+        self.assertAlmostEqual(spell_checker.get_ER([1], test_dict, ans_dict),
+                               1.0/2)
+        self.assertAlmostEqual(spell_checker.get_ER([2], test_dict, ans_dict),
+                               1.0/3)
+
+    def test_get_HM(self):
+        self.assertAlmostEqual(spell_checker.get_HM(3, 3),
+                               3, 1)
+        self.assertAlmostEqual(spell_checker.get_HM(3, 0),
+                               0, 1)
+        self.assertAlmostEqual(spell_checker.get_HM(0.3, 0.7),
+                               0.42, 1)
+        self.assertAlmostEqual(spell_checker.get_HM(0.5, 0.5),
+                               0.5 , 1)
+        self.assertAlmostEqual(spell_checker.get_HM(0.1, 0.9),
+                               0.18, 1)
+
+    def test_generate_suggestions_and_posteriors(self):
+        query = 'foo'
+        suggestions = self.spell_checker.generate_suggestions_and_posteriors(query)
+        expected = [('believe', 0.11),
+                    ('buoyant', 0.02),
+                    ('committed', 0.14999999999999999),
+                    ('distract', 0.040000000000000001),
+                    ('ecstacy', 0.040000000000000001),
+                    ('fairy', 0.050000000000000003),
+                    ('hello', 0.02),
+                    ('gracefully', 0.059999999999999998),
+                    ('liaison', 0.070000000000000007),
+                    ('occasion', 0.10000000000000001),
+                    ('possible', 0.01),
+                    ('throughout', 0.029999999999999999),
+                    ('volley', 0.070000000000000007),
+                    ('tattoos', 0.040000000000000001),
+                    ('respect', 0.19)
+                    ]
+        self.assertEqual(suggestions,
+                         expected)
+
+    def test_run_spell_check(self):
+        self.spell_checker.run_spell_check(['yo', 'boyz'])
+        self.assertEqual(self.spell_checker.suggestion_dict['yo'],
+                         self.spell_checker.generate_suggestions_and_posteriors('yo'))
+        self.assertEqual(self.spell_checker.suggestion_dict['boyz'],
+                         self.spell_checker.generate_suggestions_and_posteriors('boyz'))
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(SpellCheckerTest)
