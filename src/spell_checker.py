15dfa21e2975460e9e2cb342ef1767dc87a1c3db
S Pradeep Kumar <gohanpra@gmail.com>
2012-09-30 18:02:34 +0530

    Added partition, is_sorted, and get_splits methods.
    Filled out generate_suggestions_and_posteriors method. Yet to test it.

--- src/spell_checker.py
+++ src/spell_checker.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import itertools
+
 # Program to correct spelling errors.
 
 def get_EP(query_list, suggestion_dict, human_suggestion_dict):
@@ -33,6 +35,43 @@ def get_HM(x, y):
     """Return HM of x and y."""
     return (2 * x * y) / (x + y)
 
+def partition(given_list, indices):
+    """Return list of sublists of given_list when partitioned at indices.
+
+    indices: list (not tuple) of indices
+    http://stackoverflow.com/questions/1198512/split-a-list-into-parts-based-on-a-set-of-indexes-in-python
+    """
+    return [given_list[i:j] for i, j in zip([0]+indices, indices+[None])]
+
+def is_sorted(given_list, key = lambda x: x):
+    """Return True iff given_list is sorted.
+    """
+    return all([key(given_list[i]) <= key(given_list[i + 1]) 
+                for i in xrange(len(given_list) - 1)])
+
+def get_splits(run_on_word, num_splits):
+    """Return all possible valid splits for run_on_word.
+
+    TODO: Check for validity of each word.
+    TODO: Use DP and memoize valid word splits for substrings.
+    """
+    # List of all valid indices that a string can be partitioned at.
+    all_indices = range(1, len(run_on_word))
+
+    valid_split_indices = itertools.combinations(all_indices, num_splits)
+    valid_split_indices = [list(tuple_) for tuple_ in valid_split_indices]
+    print valid_split_indices
+    return [partition(run_on_word, index_list) 
+            for index_list in valid_split_indices]
+
+def get_corrected_run_on_queries(query):
+    """Return list of phrases/sentences queries with any run-on word split.
+
+    Assumption: max three words have been joined together.
+    Arguments:
+    - `query`: list of word(s)
+    """
+    pass
 
 class SpellChecker(object):
     """Suggest corrections for errors in queries.
@@ -41,7 +80,6 @@ class SpellChecker(object):
     def __init__(self):
         """Initialize all the dicts for suggestions, etc.
         """
-        print 'Yo, boyz!'
         self.suggestion_dict = {}
 
     def run_spell_check(self, query_list):
@@ -52,22 +90,41 @@ class SpellChecker(object):
         """
         self.query_list = query_list
         for query in self.query_list:
-            self.suggestion_dict[query] = self.generate_suggestions_and_posteriors(query)
-
+            self.suggestion_dict[query] = self.generate_suggestions_and_posteriors(
+                query)
+    
+    # To be tested once the rest is done 
     def generate_suggestions_and_posteriors(self, query):
         """Return (suggestion, posterior) pairs for query.
 
-        Arguments:
-        - `query`:
-        """
-        suggestions = ["believe", "buoyant", "committed", "distract", 
-                       "ecstacy", "fairy", "hello", "gracefully", 
-                       "liaison", "occasion", "possible", "throughout", 
-                       "volley", "tattoos", "respect"]
+        Get a list of candidate suggestions and calculate posteriors
+        for each of them.
 
-        posteriors = [0.11, 0.02, 0.15, 0.04, 0.04, 0.05, 0.02, 0.06, 0.07, 
-                      0.1, 0.01, 0.03, 0.07, 0.04, 0.19]
-        return zip(suggestions, posteriors)
+        Arguments:
+        - `query`: list of word(s) making up the word/phrase/sentence
+          query.
+        """
+        # List of list of possibilities for each term
+        all_term_possibilities = [generate_candidate_terms(term) for term in query]
+
+        # Lust of all suggestions
+        all_suggestions = generate_candidate_suggestions(all_term_combinations)
+
+        posteriors = [get_posterior(suggestion, query) 
+                      for suggestion in all_suggestions]
+
+        normalized_posteriors = get_normalized_probabilities(posteriors)
+
+        return zip(all_suggestions, normalized_posteriors)
+        
+        # suggestions = ["believe", "buoyant", "committed", "distract", 
+        #                "ecstacy", "fairy", "hello", "gracefully", 
+        #                "liaison", "occasion", "possible", "throughout", 
+        #                "volley", "tattoos", "respect"]
+
+        # posteriors = [0.11, 0.02, 0.15, 0.04, 0.04, 0.05, 0.02, 0.06, 0.07, 
+        #               0.1, 0.01, 0.03, 0.07, 0.04, 0.19]
+        # return zip(suggestions, posteriors)
 
     def get_EF1_measure(self, human_suggestion_dict):
         """Return EF1 value for the performance as judged by human_suggestion_dict.
@@ -79,7 +136,7 @@ class SpellChecker(object):
         args = [self.query_list, self.suggestion_dict, human_suggestion_dict]
         return get_HM (get_EP(*args),
                        get_ER(*args))
-
+    
     def get_all_stats(self, human_suggestion_dict):
         """Return [EP, ER, EF1] for performance as judged by human_suggestion_dict.
         
--- src/test_spell_checker.py
+++ src/test_spell_checker.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import spell_checker
 import unittest
 
@@ -40,34 +42,100 @@ class SpellCheckerTest(unittest.TestCase):
         self.assertAlmostEqual(spell_checker.get_HM(0.1, 0.9),
                                0.18, 1)
 
-    def test_generate_suggestions_and_posteriors(self):
-        query = 'foo'
-        suggestions = self.spell_checker.generate_suggestions_and_posteriors(query)
-        expected = [('believe', 0.11),
-                    ('buoyant', 0.02),
-                    ('committed', 0.14999999999999999),
-                    ('distract', 0.040000000000000001),
-                    ('ecstacy', 0.040000000000000001),
-                    ('fairy', 0.050000000000000003),
-                    ('hello', 0.02),
-                    ('gracefully', 0.059999999999999998),
-                    ('liaison', 0.070000000000000007),
-                    ('occasion', 0.10000000000000001),
-                    ('possible', 0.01),
-                    ('throughout', 0.029999999999999999),
-                    ('volley', 0.070000000000000007),
-                    ('tattoos', 0.040000000000000001),
-                    ('respect', 0.19)
-                    ]
-        self.assertEqual(suggestions,
-                         expected)
+    def test_partition(self):
+        given_list = [1, 2, 3, 4, 5, 6, 7, 8]
+        indices1 = [1]
+        indices2 = [1, 5]
+        indices3 = [1, 3, 5]
+        ans1 = [[1], [2, 3, 4, 5, 6, 7, 8]]
+        ans2 = [[1], [2, 3, 4, 5], [6, 7, 8]]
+        ans3 = [[1], [2, 3], [4, 5], [6, 7, 8]]
+        
+        self.assertEqual(spell_checker.partition(given_list, indices1),
+                         ans1)
+        self.assertEqual(spell_checker.partition(given_list, indices2),
+                         ans2)
+        self.assertEqual(spell_checker.partition(given_list, indices3),
+                         ans3)
+        
+    def test_is_sorted(self):
+        sorted_list = [2, 3, 6, 7]
+        unsorted_list = [3, 2, 6, 7]
+        self.assertTrue(spell_checker.is_sorted(sorted_list))
+        self.assertFalse(spell_checker.is_sorted(unsorted_list))
+        
+    def test_get_splits(self):
+        run_on_word = 'giantkick'
+        ans = ['giant', 'kick']
+        ans = [['g', 'i', 'antkick'],
+               ['g', 'ia', 'ntkick'],
+               ['g', 'ian', 'tkick'],
+               ['g', 'iant', 'kick'],
+               ['g', 'iantk', 'ick'],
+               ['g', 'iantki', 'ck'],
+               ['g', 'iantkic', 'k'],
+               ['gi', 'a', 'ntkick'],
+               ['gi', 'an', 'tkick'],
+               ['gi', 'ant', 'kick'],
+               ['gi', 'antk', 'ick'],
+               ['gi', 'antki', 'ck'],
+               ['gi', 'antkic', 'k'],
+               ['gia', 'n', 'tkick'],
+               ['gia', 'nt', 'kick'],
+               ['gia', 'ntk', 'ick'],
+               ['gia', 'ntki', 'ck'],
+               ['gia', 'ntkic', 'k'],
+               ['gian', 't', 'kick'],
+               ['gian', 'tk', 'ick'],
+               ['gian', 'tki', 'ck'],
+               ['gian', 'tkic', 'k'],
+               ['giant', 'k', 'ick'],
+               ['giant', 'ki', 'ck'],
+               ['giant', 'kic', 'k'],
+               ['giantk', 'i', 'ck'],
+               ['giantk', 'ic', 'k'],
+               ['giantki', 'c', 'k']]
+        self.assertEqual(spell_checker.get_splits(run_on_word, 2),
+                         ans)
 
-    def test_run_spell_check(self):
-        self.spell_checker.run_spell_check(['yo', 'boyz'])
-        self.assertEqual(self.spell_checker.suggestion_dict['yo'],
-                         self.spell_checker.generate_suggestions_and_posteriors('yo'))
-        self.assertEqual(self.spell_checker.suggestion_dict['boyz'],
-                         self.spell_checker.generate_suggestions_and_posteriors('boyz'))
+    # def test_get_corrected_run_on_queries(self):
+    #     query_3_words = 'footballhalloffame'
+    #     ans_3_words = 'football hall of fame'
+    #     query_2_words = 'giantcell'
+    #     ans_2_words = 'giant cell'
+    #     self.assertEqual(spell_checker.get_corrected_run_on_queries(query_3_words),
+    #                      ans_3_words)
+    #     self.assertEqual(spell_checker.get_corrected_run_on_queries(query_2_words),
+    #                      ans_2_words)
+
+    # def test_generate_suggestions_and_posteriors(self):
+    #     query = 'foo'
+    #     suggestions = self.spell_checker.generate_suggestions_and_posteriors(query)
+    #     expected = [('believe', 0.11),
+    #                 ('buoyant', 0.02),
+    #                 ('committed', 0.14999999999999999),
+    #                 ('distract', 0.040000000000000001),
+    #                 ('ecstacy', 0.040000000000000001),
+    #                 ('fairy', 0.050000000000000003),
+    #                 ('hello', 0.02),
+    #                 ('gracefully', 0.059999999999999998),
+    #                 ('liaison', 0.070000000000000007),
+    #                 ('occasion', 0.10000000000000001),
+    #                 ('possible', 0.01),
+    #                 ('throughout', 0.029999999999999999),
+    #                 ('volley', 0.070000000000000007),
+    #                 ('tattoos', 0.040000000000000001),
+    #                 ('respect', 0.19)
+    #                 ]
+    #     self.assertEqual(suggestions,
+    #                      expected)
+
+    # def test_run_spell_check(self):
+    #     self.spell_checker.run_spell_check(['yo', 'boyz'])
+    #     self.assertEqual(self.spell_checker.suggestion_dict['yo'],
+    #                      self.spell_checker.generate_suggestions_and_posteriors('yo'))
+    #     self.assertEqual(self.spell_checker.suggestion_dict['boyz'],
+    #                      self.spell_checker.generate_suggestions_and_posteriors('boyz'))
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(SpellCheckerTest)
